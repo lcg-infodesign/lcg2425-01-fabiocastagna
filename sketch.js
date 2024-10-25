5897d0117e1ebf5f4d56b91755304436063ac38e
Fabio Castagna <fabiocastagna04@gmail.com>
2024-10-25 08:37:32 +0200

    Assignment 1

--- sketch.js
+++ sketch.js
@@ -1,17 +1,26 @@
-function preload() {
-  // put preload code here
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  const message =
-    "This is a template repository\nfor the course Laboratorio di Computergrafica\nCommunication Design, Politecnico di Milano";
-  textAlign(CENTER, CENTER);
-  textSize(16);
-  text(message, width / 2, height / 2);
-}
-
-function draw() {
-  // put drawing code here
+  createCanvas(400, 400);
+  background(255);
+  let rows = 15;  // Numero di righe
+  let cols = 4;   // Numero di colonne
+  let rectWidth = 60;
+  let rectHeight = 15;
+  let paddingX = 20;
+  let paddingY = 20;
+  
+  for (let j = 0; j < cols; j++) {
+    for (let i = 0; i < rows; i++) {
+      // Piccola variazione nella posizione e nell'altezza
+      let xOffset = random(-2, 2);
+      let yOffset = random(-3, 3);
+      let hOffset = random(-2, 2);
+      
+      let x = paddingX + j * (rectWidth + 20) + xOffset;
+      let y = paddingY + i * (rectHeight + 10) + yOffset;
+      
+      fill(255, 0, 0);
+      noStroke();
+      rect(x, y, rectWidth, rectHeight + hOffset);
+    }
+  }
 }
